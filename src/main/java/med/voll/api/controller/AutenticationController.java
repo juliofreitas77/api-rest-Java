4a9b93c5eeca8628deec8002bbdff36ff3e9fc82
Julio Cesar S Freitas <julio.si.freitas@gmail.com>
2024-10-21 16:24:43 -0300

    Implementando Json Web Token

--- src/main/java/med/voll/api/controller/AutenticationController.java
+++ src/main/java/med/voll/api/controller/AutenticationController.java
@@ -2,6 +2,9 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.domain.usuario.DadosAutentication;
+import med.voll.api.domain.usuario.Usuario;
+import med.voll.api.infra.security.TokenDadosJWT;
+import med.voll.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,14 +18,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("login")
 public class AutenticationController {
 
+    private AuthenticationManager manager;
+    private TokenService tokenService;
+
     @Autowired
-    public AuthenticationManager manager;
+    private void setTokenService(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+
+    @Autowired
+    private void setAutenticantionManager(AuthenticationManager manager){
+        this.manager = manager;
+    }
 
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutentication dados){
-        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
-        var authentication =  manager.authenticate(token);
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var authentication =  manager.authenticate(authenticationToken);
+        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(new TokenDadosJWT(tokenJWT));
     }
+
+
 }
